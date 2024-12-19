e98ba8ac42c5918302d8883654d789a098537ef4
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-12-19 16:53:17 +0900

    Qualtrics

--- authenticator_1.py
+++ authenticator_1.py
@@ -4,11 +4,7 @@ import json
 
 class Authenticator:
     def __init__(self):
-        self.GROUP_URLS = {
-            "c": "https://main-c-hulfrzmcmnhcahoxdpc2gc.streamlit.app/",
-            "r": "https://main-c-ey8rrf82hhhzsur2wrhmx9.streamlit.app/",
-            "b": "https://main-c-chatbot-gf6b8fhdhdenreey45xtri.streamlit.app/",
-        }
+        self.BASE_URL = "https://predictchoices-mr3y9dfar8ypf66dszgrxr.streamlit.app/"
         # 特別なURLを定義
         self.SPECIAL_URL = "https://www.google.com"
 
@@ -34,26 +30,26 @@ class Authenticator:
                     "User IDを入力してください", placeholder="Enter", key="temp_user_id"
                 )
 
-    def make_user_url(self, addresed_accounts, attendance_list, group_urls):
+    def make_user_url(self, addresed_accounts, attendance_list):
         if "user_id" in st.session_state:
             if st.session_state.user_id in addresed_accounts:
                 attendance_attributes = attendance_list[st.session_state.user_id]
-                group_name = attendance_attributes[0]
-                theme = attendance_attributes[1]
-                if group_name in group_urls:
-                    group_url = group_urls[group_name]
-                    group_url_with_id = f"{group_url}?user_id={st.session_state.user_id}&group={group_name}&talktheme={theme}"
-                    st.success(f"ようこそ、{st.session_state.user_id} さん！")
-                    st.markdown(
-                        f"こちらのリンクをクリックして、今日の会話を開始してください。: <a href='{group_url_with_id}' target='_blank'>リンク</a>",
-                        unsafe_allow_html=True,
-                    )
+                theme = attendance_attributes[0]
+                group_url = self.BASE_URL
+                group_url_with_id = (
+                    f"{group_url}?user_id={st.session_state.user_id}&talktheme={theme}"
+                )
+                st.success(f"ようこそ、{st.session_state.user_id} さん！")
+                st.markdown(
+                    f"こちらのリンクをクリックして、今日の会話を開始してください。: <a href='{group_url_with_id}' target='_blank'>リンク</a>",
+                    unsafe_allow_html=True,
+                )
 
     def forward(self):
         st.title("Authentication")
         addresed_accounts, attendance_list = self.get_attendance_attributes()
         self.vertify_user_id(addresed_accounts)
-        self.make_user_url(addresed_accounts, attendance_list, self.GROUP_URLS)
+        self.make_user_url(addresed_accounts, attendance_list)
 
 
 if __name__ == "__main__":
