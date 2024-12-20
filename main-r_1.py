07b07710a161564a83de10f1ea9de82ef9cc2272
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-12-20 14:54:16 +0900

    Update

--- main-r_1.py
+++ main-r_1.py
@@ -135,6 +135,7 @@ class MainR:
             st.session_state.chat_history = []
 
         if "vector_db" not in st.session_state:
+            st.error(f"{theme}のベクトルデータベースの読み込みに失敗しました")
             chroma_db_path = f"vector_database/{theme}"
             st.session_state.vector_db = Chroma(
                 persist_directory=chroma_db_path,
